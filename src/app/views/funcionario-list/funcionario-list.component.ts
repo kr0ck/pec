241402b54b11266a61965f7d14f29684c8912f4d
Kr0ck <hebertcampos@hotmail.com.br>
2018-12-11 17:39:56 -0300

    retirando tag

--- src/app/views/funcionario-list/funcionario-list.component.ts
+++ src/app/views/funcionario-list/funcionario-list.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FuncionarioListComponent implements OnInit {
 
   elements: any = [
-    {id: 1, name: 'Fulano 1', cargo: 'Cargo 1', funcao: 'Função 1', icons: ""},
+    {id: 1, name: 'Fulano 1', cargo: 'Cargo 1', funcao: 'Função 1'},
     {id: 2, name: 'Fulano 2', cargo: 'Cargo 2', funcao: 'Função 2'},
     {id: 3, name: 'Fulano 3', cargo: 'Cargo 3', funcao: 'Função 3'},  ];
 
