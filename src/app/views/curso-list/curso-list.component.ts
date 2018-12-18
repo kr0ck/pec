514b38173c7890c7ef0aa1c3228780cb602a0c75
Kr0ck <hebertcampos@hotmail.com.br>
2018-12-18 20:01:53 -0300

    lista cursos

--- src/app/views/curso-list/curso-list.component.ts
+++ src/app/views/curso-list/curso-list.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoListComponent implements OnInit {
 
+  elements: any = [
+    {id: 1, name: 'Curso 1', modalidade: 'Modalidade 1', cargHora: '1'},
+    {id: 2, name: 'Curso 2', modalidade: 'Modalidade 2', cargHora: '2'},
+    {id: 3, name: 'Curso 3', modalidade: 'Modalidade 3', cargHora: '3'},  ];
+
+  headElements = ['ID', 'Nome', 'Modalidade', 'Carga Horária', ''];
+
+
+
   constructor() { }
 
   ngOnInit() {
